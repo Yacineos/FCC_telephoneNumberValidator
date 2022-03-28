fb941c2ff2efe91b16e25c7ca8145b5555dc20d1
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-03-28 21:14:15 +0100

    rules and figuring out the prblm

--- telephoneNumVal.js
+++ telephoneNumVal.js
@@ -16,3 +16,36 @@ function parenthesis(S) {
   }
 }
 
+//verify parenthesis with the function x
+//function to remove spacing and - and the parenthesis
+// if the the first char is 1 then must be length 11 else 10
+// first condition char in the string are numbers , parenthesis or -
+// second condition verify the beginning 1 
+// fourth condition verifyi spacing between numbers 
+
+function telephoneCheck(str){
+    var temp ;
+    if(parenthesis(str)==false){
+            return false ;
+    }else{
+        temp=str.replace(/[^0-9]/g, '');
+        if(temp[0]=='1'){
+            if(temp.length!=11){
+                return false ;
+            }else{
+                
+
+            }
+
+        }else{
+            if(temp.length!=10){
+                return false;
+            }else{
+
+            }
+
+        }
+    }
+}
+
+telephoneCheck("555-555-5555");
